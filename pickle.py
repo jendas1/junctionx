54ab86a0940918dc469c925ed4af15de43a227ab
Stanislav Zakrevskii <s.zakrevskiy@iwoca.co.uk>
2020-02-21 23:57:30 +0000

    added pickling interface

---
+++ pickle.py
@@ -0,0 +1,22 @@
+def get_full_user_data(user_id):
+    pass
+
+def save_user_data(user_id):
+    pass
+
+def add_user_mood(user_id: int, mood: int):
+    pass
+
+def get_user_mood(user_id: int):
+    pass
+
+def set_custom_data(user_id, key, data):
+    pass
+
+def purge():
+    pass
+
+def clean_user_data(user_id: int):
+    pass
+
+
