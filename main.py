c9d65819a84b571e2bd669c7cac821771e9b4f84
Stanislav Zakrevskii <s.zakrevskiy@iwoca.co.uk>
2020-02-22 12:16:14 +0000

    Fix emoji analyzer, added next command

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 import logging
+from datetime import datetime
+
 from matplotlib.pyplot import imread, imsave
 import numpy as np
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pickler import get_full_user_data, save_user_data, save_all, load_all
 from json import load, dump
 
-TOKEN="868031376:AAGdASZ8GANAa3L5nyHZEZiHX4Yais8_DCg"
+BAD_EMOTION_THRESHOLD = 4
+TOKEN = "868031376:AAGdASZ8GANAa3L5nyHZEZiHX4Yais8_DCg"
+os.environ['TOKEN'] = TOKEN
 updater = Updater(token=os.environ.get('TOKEN'), use_context=True)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -15,9 +19,9 @@ questions = [
     '',
     'How do you feel right now?',
     'Have you helped anyone today? How?',
-    'What are you grateful for?'
+    'What are you grateful for?',
+    "How is your mood today? (answer with emoji)"
 ]
-
 
 is_in_mood = set()
 
@@ -31,8 +35,17 @@ def start(update, context):
         text="I'm a bot, please send me a selfie to start!")
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def next_day(update, context):
+    username = update.effective_chat.username
+    user_data = load_all()
+    if username not in user_data:
+        user_data[username] = {'answers': {}}
+    if 'archive' not in user_data[username]:
+        user_data[username]['archive'] = {}
+    user_data[username]['archive'][datetime.now()] = user_data[username]['answers']
+    user_data[username]['answers'] = {}
+    save_all(user_data)
+    start(update, context)
 
 
 def selfie(update, context):
@@ -56,23 +69,21 @@ def selfie(update, context):
         text='Would you like to start mental state questionnaire now?')
 
 
-selfie_handler = MessageHandler(Filters.photo, selfie)
-dispatcher.add_handler(selfie_handler)
-
-
 def update_photo(filename):
     image = imread(filename)
     lum_img = image[:, :, ::-1]
     imsave(filename, lum_img)
 
-BAD_EMOTION_THRESHOLD = 7
 
 def emotionizer(answer):
     score = emojimap.get(answer[0:2], None)
     if score is None:
         score = emojimap.get(answer[0:1], None)
     if score is None:
-        return 0
+        return 0.
+    else:
+        return float(score.get('score', 0.))
+
 
 def questionnaire(update, context):
     username = update.effective_chat.username
@@ -86,14 +97,17 @@ def questionnaire(update, context):
         answer = update.message.text
         is_in_mood.remove(username)
         user_emotion = emotionizer(answer)
+        logging.info(f'emotion score is {user_emotion}')
         emotions = user_data[username].get("emotions", [])
         emotions.append(user_emotion)
-        user_data["emotions"] = emotions
+        user_data[username]["emotions"] = emotions
         if len(emotions) >= BAD_EMOTION_THRESHOLD:
             state_bad = is_everything_bad(emotions)
             if state_bad:
                 context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text="Hey! You've been constantly in a bad mood for 7 days. Maybe you should talk to your relatives or seek some aid?")
+                                         text="Hey! You've been constantly in a bad mood for 4 days. Maybe you should talk to your relatives or seek some aid?")
+    answers = user_data[username]['answers']
+    save_all(user_data)
 
     if current_answer_num + 2 >= len(questions):
         context.bot.send_message(
@@ -102,23 +116,29 @@ def questionnaire(update, context):
         update_photo(f'{username}.jpg')
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(f'{username}.jpg', 'rb'))
         return
-    answers = user_data[username]['answers']
     if len(answers) < len(questions):
         bot_question = questions[len(answers)]
         if bot_question.split()[3] == "mood":
             is_in_mood.add(username)
         context.bot.send_message(chat_id=update.effective_chat.id, text=bot_question)
-    save_all(user_data)
     logging.info(f'{username} {user_data}')
 
 
 def is_everything_bad(emotions):
     for emotion in emotions[-BAD_EMOTION_THRESHOLD:]:
         if emotion >= 0:
-            return True
-    return False
+            return False
+    return True
 
-echo_handler = MessageHandler(Filters.text, questionnaire)
-dispatcher.add_handler(echo_handler)
 
-updater.start_polling()
+if __name__ == '__main__':
+    start_handler = CommandHandler('start', start)
+    next_day_handler = CommandHandler('next', next_day)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(next_day_handler)
+    selfie_handler = MessageHandler(Filters.photo, selfie)
+    dispatcher.add_handler(selfie_handler)
+    echo_handler = MessageHandler(Filters.text, questionnaire)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
