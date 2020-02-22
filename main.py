101f8937ecee5b2d8a686815c918a1342b27ace9
Ivan Savin <acccko@gmail.com>
2020-02-22 09:54:16 +0000

    image manipulation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import logging
+from matplotlib.pyplot import imread, imsave
+import numpy as np
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
@@ -20,11 +22,43 @@ user_data = {}
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please send me a selfie to start!")
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+
+def selfie(update, context):
+    file = context.bot.getFile(update.message.photo[-1].file_id)
+    file.download('image.png')
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Please upload your selfie to start")
+
+
+def selfie(update, context):
+    username = update.effective_chat.username
+    if update.message.photo:
+        file = context.bot.getFile(update.message.photo[-1].file_id)
+        file.download(f'{username}.jpg')
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Selfie saved, thanks!')
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Would you like to start mental state questionnaire now?')
+
+
+selfie_handler = MessageHandler(Filters.photo, selfie)
+dispatcher.add_handler(selfie_handler)
+
+
+def update_photo(filename):
+    image = imread(filename)
+    lum_img = image[:,:,::-1]
+    imsave(filename, lum_img)
 
 
 def questionnaire(update, context):
@@ -37,7 +71,9 @@ def questionnaire(update, context):
     if current_answer_num + 2 >= len(questions):
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text='You have answered all the questions for today, thanks!')
+            text='You have answered all the questions for today. Here is how your portrait look like')
+        update_photo(f'{username}.jpg')
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(f'{username}.jpg', 'rb'))
         return
     answers = user_data[username]['answers']
     if len(answers) < len(questions):
@@ -47,5 +83,6 @@ def questionnaire(update, context):
 
 echo_handler = MessageHandler(Filters.text, questionnaire)
 dispatcher.add_handler(echo_handler)
+
 
 updater.start_polling()
